abd5cf97de3aae4d101e54cdcfd239e4be3947b1
Gonzalo <gonzalo.yarvi@unc.edu.ar>
2022-06-23 21:20:45 -0300

    Mejoras en intensidad de la luz

--- main/analog_in.cpp
+++ main/analog_in.cpp
@@ -17,21 +17,21 @@ int analogInUpdate()
   adcValue = analogRead(analogPin); /* Read the Analog Input value */
 
   if( adcValue <= 15 ) { adcValue = 0;}
-  if( adcValue > 15 && adcValue <= 25 ) { adcValue = 0;}
-  if( adcValue > 25 && adcValue <= 30  ) { adcValue = 2;}    
-  if( adcValue > 30 && adcValue <= 40  ) { adcValue = 4;}
-  if( adcValue > 40 && adcValue <= 50  ) { adcValue = 6;}
-  if( adcValue > 50 && adcValue <= 60 ) { adcValue = 8;} 
-  if( adcValue > 60 && adcValue <= 70  ) { adcValue = 10;}
-  if( adcValue > 70 && adcValue <= 80  ) { adcValue = 14;}
-  if( adcValue > 80 && adcValue <= 90  ) { adcValue = 18;} 
-  if( adcValue > 80 && adcValue <= 100  ) { adcValue = 24;}
-  if( adcValue > 100 && adcValue <= 110  ) { adcValue = 30;}
-  if( adcValue > 110 && adcValue <= 120  ) { adcValue = 40;} 
-  if( adcValue > 120 && adcValue <= 130  ) { adcValue = 60;}
-  if( adcValue > 130 && adcValue <= 140  ) { adcValue = 80;}
-  if( adcValue > 140 && adcValue <= 150  ) { adcValue = 100;}
-  if( adcValue > 150 && adcValue <= 160  ) { adcValue = 140;}
+  if( adcValue > 15 && adcValue <= 25 ) { adcValue = 1;}
+  if( adcValue > 25 && adcValue <= 50  ) { adcValue = 2;}    
+  if( adcValue > 50 && adcValue <= 75  ) { adcValue = 3;}
+  if( adcValue > 75 && adcValue <= 100  ) { adcValue = 4;}
+  if( adcValue > 100 && adcValue <= 150 ) { adcValue = 5;} 
+  if( adcValue > 150 && adcValue <= 200  ) { adcValue = 6;}
+  if( adcValue > 200 && adcValue <= 250  ) { adcValue = 7;}
+  if( adcValue > 250 && adcValue <= 300  ) { adcValue = 8;} 
+  if( adcValue > 300 && adcValue <= 400  ) { adcValue = 9;}
+  if( adcValue > 400 && adcValue <= 500  ) { adcValue = 10;}
+  if( adcValue > 500 && adcValue <= 600  ) { adcValue = 40;} 
+  if( adcValue > 600 && adcValue <= 700  ) { adcValue = 60;}
+  if( adcValue > 700 && adcValue <= 800  ) { adcValue = 80;}
+  if( adcValue > 800 && adcValue <= 900  ) { adcValue = 100;}
+  if( adcValue > 900 ) { adcValue = 1000;}
 
 
   delay(10);
